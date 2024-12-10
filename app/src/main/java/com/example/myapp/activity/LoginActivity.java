7ac2bc791a3da59e6590b44041dcad1b28fe9899
czk23 <2071895341@qq.com>
2024-12-10 16:48:22 +0800

    完成登录界面

--- app/src/main/java/com/example/myapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/myapp/activity/LoginActivity.java
@@ -1,9 +1,6 @@
-package com.example.myapp;
+package com.example.myapp.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,31 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.myapp.activity.LoginActivity;
+import com.example.myapp.R;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button button_Login;
-    private Button button_Register;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        button_Login = findViewById(R.id.button_login);
-        button_Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent in = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(in);
-            }
         });
     }
 }
